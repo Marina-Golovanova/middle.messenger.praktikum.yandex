8e1bb9e6763569bcac03dd7e710518719ca8beb7
Marina Golovanova <gma18@list.ru>
2023-10-23 19:08:03 +0100

    [FEAT] Add form-layout, input, input-readonly, authorization-layout, login, registraion-form; [FIX] add default style to button

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,27 @@ import postcssImport from 'postcss-import';
 export default defineConfig({
   plugins: [
     handlebars({
-      partialDirectory: ['./src/components/button'],
+      partialDirectory: [
+        './src/components/button',
+        './src/components/input',
+        './src/components/form-layout',
+        './src/pages/authorization/modules/login',
+        './src/pages/registration/modules/registration-form',
+        './src/layouts/authorization-layout',
+      ],
       context: {
-        title: 'Hello, world!',
+        loginFields: [
+          { label: 'login', placeholder: 'example@yandex.ru' },
+          { label: 'password' },
+        ],
+        registrationFields: [
+          { label: 'first name', placeholder: 'Anna' },
+          { label: 'last name', placeholder: 'Smith' },
+          { label: 'email', placeholder: 'example@yandex.com' },
+          { label: 'password' },
+          { label: 'repeat password' },
+          { label: 'phone', placeholder: '+7-999-888-77-66' },
+        ],
       },
     }) as Plugin,
   ],
