a42c1c8b8ee907c3a8410d8308c13963c2563c6e
Marina Golovanova <gma18@list.ru>
2023-11-14 19:56:47 +0100

    [REFACTOR]

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,9 @@ import handlebars from 'vite-plugin-handlebars';
 import postcssNested from 'postcss-nested';
 import postcssImport from 'postcss-import';
 import checker from 'vite-plugin-checker';
-import { pages } from './pagesConfig';
 import path from 'path';
 
-const pagesInput = {};
-
-pages.forEach((page) => {
-  pagesInput[page.name] = page.path;
-});
-
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        ...pagesInput,
-      },
-    },
-  },
   plugins: [
     handlebars() as Plugin,
     checker({
